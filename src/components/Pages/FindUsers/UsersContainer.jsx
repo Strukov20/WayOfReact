81804e002b081cd5675a5a16766e484a6fb0a20f
Bohdan Strukov <bohdan.strukov@developex.local>
2021-08-28 00:59:00 +0300

    rafactoring users component + add usersAPIComponent

--- src/components/Pages/FindUsers/UsersContainer.jsx
+++ src/components/Pages/FindUsers/UsersContainer.jsx
@@ -1,6 +1,37 @@
 import Users from "./Users";
 import {connect} from "react-redux";
 import {followingAC, setCurrentPage, setUsers} from "../../../Redux/Reducers/users-reducer";
+import React, {Component} from "react";
+import * as axios from "axios";
+
+class UsersAPIContainer extends Component{
+    getUsers = (nextPage) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${nextPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            this.getUsers()
+        }
+
+    }
+
+    showMoreUsers = () => {
+        let nextPage = this.props.currentPage + 1
+        this.props.setCurrentPage(nextPage)
+        this.getUsers(nextPage)
+    }
+    render() {
+        return <Users pageSize={this.props.pageSize}
+                      following={this.props.following}
+                      pageName={this.props.pageName}
+                      showMoreUsers={this.showMoreUsers}
+                      users={this.props.users}/>
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -26,6 +57,6 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIContainer)
 
 export default UsersContainer;
